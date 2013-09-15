8c22c7b81932705729491ab99c808e21e8c827fd
Islam Sharabash <islam.sharabash@gmail.com>
2013-09-15 15:16:44 -0700

    remove trailing spaces

--- jquery.ajaxfileupload.js
+++ jquery.ajaxfileupload.js
@@ -26,17 +26,17 @@
 
         var uploading_file = false;
 
-        if ( options ) { 
+        if ( options ) {
           $.extend( settings, options );
         }
 
 
-        // 'this' is a jQuery collection of one or more (hopefully) 
+        // 'this' is a jQuery collection of one or more (hopefully)
         //  file elements, but doesn't check for this yet
         return this.each(function() {
           var $element = $(this);
 
-          // Skip elements that are already setup. May replace this 
+          // Skip elements that are already setup. May replace this
           //  with uninit() later, to allow updating that settings
           if($element.data('ajaxUploader-setup') === true) return;
 
@@ -78,10 +78,10 @@
               // Pass back to the user
               settings.onComplete.apply($element, [{status: false, message: 'The select file type is invalid. File must be ' + settings.valid_extensions.join(', ') + '.'}, settings.params]);
             } else
-            { 
+            {
               uploading_file = true;
 
-              // Creates the form, extra inputs and iframe used to 
+              // Creates the form, extra inputs and iframe used to
               //  submit / upload the file
               wrapElement($element);
 
@@ -101,8 +101,8 @@
           $element.data('ajaxUploader-setup', true);
 
           /*
-          // Internal handler that tries to parse the response 
-          //  and clean up after ourselves. 
+          // Internal handler that tries to parse the response
+          //  and clean up after ourselves.
           */
           var handleResponse = function(loadedFrame, element) {
             var response, responseStr = settings.dataType == 'json' ? loadedFrame.contentWindow.document.body.innerText : loadedFrame.contentWindow.document.body.innerHTML;
@@ -126,8 +126,8 @@
           /*
           // Wraps element in a <form> tag, and inserts hidden inputs for each
           //  key:value pair in settings.params so they can be sent along with
-          //  the upload. Then, creates an iframe that the whole thing is 
-          //  uploaded through. 
+          //  the upload. Then, creates an iframe that the whole thing is
+          //  uploaded through.
           */
           var wrapElement = function(element) {
             // Create an iframe to submit through, using a semi-unique ID
